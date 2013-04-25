2c0116290c6017c8cad06eec1795be5aa61c7f57
Peter Odding <peter.odding@paylogic.eu>
2013-04-25 12:31:45 +0200

    Document missing dependency of python-debian

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(name='pl-py2deb',
-      version='0.5.1',
+      version='0.5.2',
       description='A tool to convert python packages to debian packages.',
       author='Arjan Verwer',
       author_email='arjan.verwer@paylogic.eu',
       url='https://wiki.paylogic.eu/',
       packages=find_packages(),
       package_data={'py2deb': ['config/*.ini']},
-      install_requires=['python-debian', 'stdeb'],
+      install_requires=[
+          'python-debian', # proper dependency
+          'stdeb', # proper dependency
+          'chardet', # transitive dependency of `python-debian'... (no one gets dependencies right :-)
+      ],
       entry_points={'console_scripts': ['pl-py2deb = py2deb:main']})
