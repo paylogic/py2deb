fd061738d025b5371c1415a1f5466bcf5f6476b7
Peter Odding <peter.odding@paylogic.eu>
2013-04-25 12:49:02 +0200

    Make it work out of the box on the build-server and locally

--- py2deb/config/__init__.py
+++ py2deb/config/__init__.py
@@ -3,4 +3,7 @@ import os
 config_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Destination of built packages.
-PKG_REPO = '/tmp/'
+if os.getuid() == 0:
+    PKG_REPO = '/var/repos/deb-repo/repository/pl-py2deb'
+else:
+    PKG_REPO = '/tmp'
