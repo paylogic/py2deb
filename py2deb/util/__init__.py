8b099e35297e5d0ef35b6ada8fa2d604a748e833
Peter Odding <peter.odding@paylogic.eu>
2013-06-04 11:15:38 +0200

    Bug fix for latest commit (transform_package_name())

--- py2deb/util/__init__.py
+++ py2deb/util/__init__.py
@@ -1,6 +1,6 @@
 import re
 
-def transform_package_name(self, name):
+def transform_package_name(name):
     '''
     Transforms the name of a Python package as found on PyPi into the name that
     we want it to have as a Debian package (using our prefix).
