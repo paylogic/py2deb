fedc59fde63a3501511db3ac46f8dbc02c7d956a
Peter Odding <peter.odding@paylogic.eu>
2013-06-27 14:12:56 +0200

    Sneaking in a minor bug fix

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(name='pl-py2deb',
-      version='0.6.6',
-      description='A tool to convert python packages to debian packages.',
+      version='0.6.7',
+      description='Converts Python packages to Debian packages (including dependencies).',
       author='Arjan Verwer',
       author_email='arjan.verwer@paylogic.eu',
       url='https://wiki.paylogic.eu/',
