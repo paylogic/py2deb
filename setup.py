eb17be71f989401028f5f9f3b4f07bdfd6c007ef
Peter Odding <peter@peterodding.com>
2014-06-18 17:25:11 +0200

    Bug fix: Include postinst/prerm scripts during installation!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup script for the `py2deb` package.
 """
 
 # Author: Peter Odding <peter.odding@paylogic.com>
-# Last Change: June 11, 2014
+# Last Change: June 18, 2014
 # URL: https://py2deb.readthedocs.org
 
 # Standard library modules.
@@ -44,4 +44,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     test_suite='py2deb.tests',
     entry_points={'console_scripts': ['py2deb = py2deb.cli:main']},
-    install_requires=requirements)
+    install_requires=requirements,
+    include_package_data=True)
