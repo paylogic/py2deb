2cd321f8c74a3c9de2a3f10a21955d7525045563
Peter Odding <peter@peterodding.com>
2018-02-24 17:50:55 +0100

    Release 1.1: Support conditional dependencies via environment markers

--- py2deb/__init__.py
+++ py2deb/__init__.py
@@ -3,7 +3,7 @@
 # Authors:
 #  - Arjan Verwer
 #  - Peter Odding <peter.odding@paylogic.com>
-# Last Change: August 8, 2017
+# Last Change: February 24, 2018
 # URL: https://py2deb.readthedocs.io
 
 """
@@ -15,4 +15,4 @@ The top level :mod:`py2deb` module contains only a version number.
 """
 
 # Semi-standard module versioning.
-__version__ = '1.0'
+__version__ = '1.1'
