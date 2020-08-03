paylogic/py2deb

commit 5074036ec0248bc28f6334f00e85e937b7698bb0

Disable wheels on PyPy 3 (getting desperate 😅)

Makefile
@@ -12,6 +12,42 @@ PATH := $(VIRTUAL_ENV)/bin:$(PATH)
 MAKE := $(MAKE) --no-print-directory
 SHELL = bash
 
+# Configure pip not to use binary wheels on PyPy 3 (with the goal of
+# stabilizing Travis CI builds). I struggled to work around several issues
+# involving pip using wheels on PyPy 3 but subsequently "crashing" (tracebacks)
+# in one of several ways due to the use of wheels. After wasting quite a few
+# more hours than I care to admit on this state of affairs I decided to
+# preserve my sanity by "giving in" and just disabling wheels on PyPy 3
+# altogether. Related build failures:
+#
+#  - Job 714371868: AssertionError involving missing byte code files.
+#    https://travis-ci.org/github/paylogic/py2deb/jobs/714371868
+#
+#  - Job 714415991: IndexError involving 'setuptools' package
+#    https://travis-ci.org/github/paylogic/py2deb/jobs/714415991
+#
+#  - Job 714419661: IndexError involving 'wheel' package
+#    https://travis-ci.org/github/paylogic/py2deb/jobs/714419661
+#
+#  - Job 714421082: IndexError involving 'flake8' package
+#    https://travis-ci.org/github/paylogic/py2deb/jobs/714421082
+#
+# Here's an overview of things I tried and what I ended up using:
+# https://github.com/paylogic/py2deb/compare/0f785b8cbc3...819b8101a21
+ifeq ($(findstring pypy3,$(PYTHON)),pypy3)
+NO_BINARY_OPTION=:all:
+else
+NO_BINARY_OPTION=:none:
+endif
+
+# Define how pip is run in a single place.
+PIP_CMD := python -m pip
+
+# Define a common 'pip install' command (DRY).
+PIP_INSTALL_CMD := $(PIP) install \
+	--constraint=constraints.txt \
+	--no-binary=$(NO_BINARY_OPTION)
+
 default:
 	@echo "Makefile for $(PACKAGE_NAME)"
 	@echo
@@ -37,12 +73,12 @@ ifeq ($(TRAVIS), true)
 # install them before the main 'pip install' run the setup.py script of
 # cryptography attempts to take care of this on its own initiative which for
 # some reason fails on PyPy: https://travis-ci.org/github/paylogic/py2deb/jobs/713379963
-	@pip install --constraint=constraints.txt --no-binary=:all: --upgrade 'setuptools >= 40.6.0' wheel
-	@pip install --constraint=constraints.txt --requirement=requirements-travis.txt
+	@$(PIP_INSTALL_CMD) --upgrade 'setuptools >= 40.6.0' wheel
+	@$(PIP_INSTALL_CMD) --requirement=requirements-travis.txt
 else
-	@pip install --constraint=constraints.txt --requirement=requirements.txt
-	@pip uninstall --yes $(PACKAGE_NAME) &>/dev/null || true
-	@pip install --no-deps --ignore-installed .
+	@$(PIP_INSTALL_CMD) --requirement=requirements.txt
+	@$(PIP) uninstall --yes $(PACKAGE_NAME) &>/dev/null || true
+	@$(PIP_INSTALL_CMD) --no-deps --ignore-installed .
 endif
 
 reset:
@@ -51,31 +87,31 @@ reset:
 	@$(MAKE) install
 
 check: install
-	@pip install --upgrade --constraint=constraints.txt --requirement=requirements-checks.txt
+	@$(PIP_INSTALL_CMD) --upgrade --requirement=requirements-checks.txt
 	@flake8
 
 test: install
-	@pip install --constraint=constraints.txt --requirement=requirements-tests.txt
+	@$(PIP_INSTALL_CMD) --requirement=requirements-tests.txt
 	@py.test --cov
 	@coverage html
 	@coverage report --fail-under=90 &>/dev/null
 
 tox: install
-	@pip install --constraint=constraints.txt tox
+	@$(PIP_INSTALL_CMD) tox
 	@tox
 
 readme: install
-	@pip install cogapp
+	@$(PIP_INSTALL_CMD) cogapp
 	@cog.py -r README.rst
 
 docs: readme
-	@pip install --constraint=constraints.txt sphinx
+	@$(PIP_INSTALL_CMD) sphinx
 	@cd docs && sphinx-build -nb html -d build/doctrees . build/html
 
 publish: install
 	@git push origin && git push --tags origin
 	@$(MAKE) clean
-	@pip install --constraint=constraints.txt twine wheel
+	@$(PIP_INSTALL_CMD) twine wheel
 	@$(PYTHON) setup.py sdist bdist_wheel
 	@twine upload dist/*
 	@$(MAKE) clean

commit ea0123d16c0fcfbf3d1a71bb63f40ab6a9e4ba7f

Fix silly error in makefile (introduced in 5074036ec02)

Makefile
@@ -44,7 +44,7 @@ endif
 PIP_CMD := python -m pip
 
 # Define a common 'pip install' command (DRY).
-PIP_INSTALL_CMD := $(PIP) install \
+PIP_INSTALL_CMD := $(PIP_CMD) install \
 	--constraint=constraints.txt \
 	--no-binary=$(NO_BINARY_OPTION)
 