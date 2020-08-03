917790d677628efb5076a3daac8b352ed836d336
Peter Odding <peter@peterodding.com>
2020-08-03 09:21:09 +0200

    Tell pip to go and hara kiri itself :-p

--- scripts/downgrade-pip-on-pypy.py
+++ scripts/downgrade-pip-on-pypy.py
@@ -17,10 +17,7 @@ if platform.python_implementation() == "PyPy":
     installed_release = LooseVersion(pip.__version__)
     known_bad_release = LooseVersion("20.2")
     if installed_release >= known_bad_release:
-        # Given that pip is broken, we can't use it to downgrade itself!
-        # Fortunately setuptools provides easy_install which works fine.
-        # We use --always-unzip in an attempt to ensure that easy_install
-        # replaces the current pip installation instead of installing a
-        # *.egg parallel to an existing regular install.
-        sys.stderr.write("Downgrading pip using easy_install ..\n")
+        sys.stderr.write("[downgrade-pip-on-pypi] Removing broken pip release ..\n")
+        subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "--yes", "pip"])
+        sys.stderr.write("[downgrade-pip-on-pypi] Downgrading pip using easy_install ..\n")
         subprocess.check_call([sys.executable, "-m", "easy_install", "--always-unzip", "pip < 20.2"])
--- scripts/pypi-in-tox.sh
+++ scripts/pypi-in-tox.sh
@@ -4,9 +4,9 @@
 # This explains why we have to use `scripts/' in the pathname below.
 
 # Downgrade to pip < 20.2 on PyPI in tox.
-echo "Pip before downgrade: $(pip --version)" >&2
+echo "[pypi-in-tox] Pip before downgrade: $(pip --version)" >&2
 python scripts/downgrade-pip-on-pypy.py
-echo "Pip after downgrade: $(pip --version)" >&2
+echo "[pypi-in-tox] Pip after downgrade: $(pip --version)" >&2
 
 # Continue installing packages as normal.
 python -m pip install "$@"
