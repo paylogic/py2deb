68c4adf425a87db9a6b02259e69f6f35be5a6d35
Peter Odding <peter@peterodding.com>
2020-08-03 16:13:17 +0200

    Typos everywhere 😒

--- scripts/pypy.sh
+++ scripts/pypy.sh
@@ -3,7 +3,7 @@
 # This shell script is run by tox from the project directory, one level up.
 # This explains why we have to use `scripts/' in the pathname below.
 
-# Downgrade to pip < 20.2 on PyPI in tox.
+# Downgrade to pip < 20.2 when running on PyPy inside tox.
 echo "[scripts/pypy.sh] pip before downgrade: $(pip --version)" >&2
 python scripts/downgrade-pip-on-pypy.py
 echo "[scripts/pypy.sh] pip after downgrade: $(pip --version)" >&2
